2169f7b85e4ba520ce0a7608e116fe68d96808e2
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2018-08-22 01:19:41 -0300

    Adicionada a ação que leva a tela de gravação!

--- app/src/main/java/br/com/lucolimac/barkscanner/MainActivity.java
+++ app/src/main/java/br/com/lucolimac/barkscanner/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                new Intent(MainActivity.this,Gravador.class);
+                startActivity(new Intent(MainActivity.this, Gravador.class));
             }
         });
     }

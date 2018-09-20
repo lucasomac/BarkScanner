880d3d5bb489d81c70350471a080a160ec738a4f
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2018-09-20 00:41:40 -0300

    Mudanças da classe de login. Colocado o spinner para os estados.

--- app/src/main/java/br/com/lucolimac/barkscanner/cadastro/CadastroUsuario.java
+++ app/src/main/java/br/com/lucolimac/barkscanner/cadastro/CadastroUsuario.java
@@ -2,14 +2,20 @@ package br.com.lucolimac.barkscanner.cadastro;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import br.com.lucolimac.barkscanner.R;
 
 public class CadastroUsuario extends AppCompatActivity {
+    private Spinner spinnerUf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+        spinnerUf = findViewById(R.id.uf);
+        String ufs[] = getResources().getStringArray(R.array.lista_uf);
+        spinnerUf.setAdapter(new ArrayAdapter<String>(getBaseContext(), R.layout.activity_cadastro_usuario, ufs));
     }
 }

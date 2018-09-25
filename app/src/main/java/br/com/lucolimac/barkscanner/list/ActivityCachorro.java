0754ecae1f8332802b21281afe07c47424707a41
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2018-09-24 23:02:44 -0300

    Alteração do SDK

--- app/src/main/java/br/com/lucolimac/barkscanner/list/ActivityCachorro.java
+++ app/src/main/java/br/com/lucolimac/barkscanner/list/ActivityCachorro.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import br.com.lucolimac.barkscanner.R;
 import br.com.lucolimac.barkscanner.model.Cachorro;
 import br.com.lucolimac.barkscanner.model.Porte;
 import br.com.lucolimac.barkscanner.model.Usuario;
+
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivityCachorro extends AppCompatActivity {
 
@@ -21,9 +21,9 @@ public class ActivityCachorro extends AppCompatActivity {
         RecyclerView view_cachorro = findViewById(R.id.view_cachorro);
         Usuario dono = new Usuario("Lucas", "lucasomac@outlook.com", "72816053", "SE", "Aracaju", "Santos Dumont", "04806586501");
         Cachorro dog = new Cachorro("Toto", "PUG", 56, dono, Porte.PEQUENO);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("cachorro");
-        myRef.setValue(dog);
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("cachorro");
+//        myRef.setValue(dog);
         //List<Cachorro> list;
     }
 }

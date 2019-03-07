19f492ba29c1cdd6ee27002ade722f10d16683b7
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2019-03-07 19:53:10 -0300

    Upgrade de versão do firebase(antes da mudança) Versões anntigas
    Firebase-database-16.0.6
    Firebase-storage-16.0.5

--- app/src/main/java/br/com/lucolimac/barkscanner/cadastro/Gravador.java
+++ app/src/main/java/br/com/lucolimac/barkscanner/cadastro/Gravador.java
@@ -198,7 +198,6 @@ public class Gravador extends AppCompatActivity {
                             // Handle failures
                             // ...
 
-
                         }
                     }
                 });
